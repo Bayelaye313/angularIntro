8fb6d25ee2c29f207f47c7188e15ab08071cd7c1
bayelaye313 <bayelaye0510@gmail.com>
2024-04-20 19:56:11 +0000

    server static avec nodejs

--- myspace-api/index.js
+++ myspace-api/index.js
@@ -1,8 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+var bodyParser = require('body-parser')
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'static')));
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/static/:filename/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'static',req.params.filename));
+})
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  const htmlcontent = `
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello Abdoulaye</h1>
+</body>
+</html>
+  `
+  const jsoncontent = {'message': 'bonjour, abdoulaye'}
+  res.set('Content-Type', 'application/json')
+  res.send(jsoncontent)
+  console.log('server params', req.params);
 })
 
 app.listen(3000);
