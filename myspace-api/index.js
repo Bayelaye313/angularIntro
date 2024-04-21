0bd9e94753ee1ef6b6fb4d97591d9cbbe87f6895
bayelaye313 <bayelaye0510@gmail.com>
2024-04-21 19:17:01 +0000

    getpostdb

--- myspace-api/index.js
+++ myspace-api/index.js
@@ -11,12 +11,33 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-app.get('/', function (req, res) {
-  const jsoncontent = {'message': 'bonjour, bayelaye'}
-  res.set('Content-Type', 'application/json')
-  res.send(jsoncontent)
-  console.log('server params', req.params);
+app.get('/utilisateurs', function (req, res) {
+  db.all("SELECT * FROM users", function(err, rows){
+    console.log('rows', rows);
+    res.set('Content-Type', 'application/json');
+    res.set('server', 'Myspace Server')
+    res.send(rows)  
+  })
 })
+app.post('/utilisateurs', function (req, res) {
+  const data = req.body;
+  const jsonusers = data;
+
+  //requete prepare
+  /*const stmt = db.prepare("insert into users(firstname, lastname, login, password) values(?,?,?,?)");
+  const stmt_query_val = (data.firstname+" " +data.lastname+" " +data.login+" " +data.password)
+  console.log('stmtval: ',stmt_query_val);
+  stmt.run(stmt_query_val);
+  stmt.finalize();*/
+  //using exec
+  const sql =(`insert into users(firstname, lastname, login, password) values("${data.firstname}", "${data.lastname}", "${data.login}", "${data.password}")`)
+  db.exec(sql)
+  //reponse
+  res.set('Content-Type', 'application/json')
+  res.set('server', 'Myspace Server')
+  res.send(jsonusers)
+})
+
 
 app.listen(3000);
 console.log('server started on port 3000')
