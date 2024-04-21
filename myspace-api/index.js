6d0dbcea879fa7563858da9bc2f24c76422240d2
bayelaye313 <bayelaye0510@gmail.com>
2024-04-21 17:10:22 +0000

    init bd

--- myspace-api/index.js
+++ myspace-api/index.js
@@ -1,35 +1,18 @@
 const express = require('express');
-const path = require('path');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+
+const db = require('./db');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'static')));
 
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/static/:filename/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'static',req.params.filename));
-})
 
 app.get('/', function (req, res) {
-  const htmlcontent = `
-  <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello Abdoulaye</h1>
-</body>
-</html>
-  `
-  const jsoncontent = {'message': 'bonjour, abdoulaye'}
+  const jsoncontent = {'message': 'bonjour, bayelaye'}
   res.set('Content-Type', 'application/json')
   res.send(jsoncontent)
   console.log('server params', req.params);
