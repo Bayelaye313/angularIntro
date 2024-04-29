8e445c5681672f35df6d39007b17962c6bedeb50
bayelaye313 <bayelaye0510@gmail.com>
2024-04-29 18:37:30 +0000

    fix timer

--- my-first-project/src/app/quiz-app/quiz-app.component.ts
+++ my-first-project/src/app/quiz-app/quiz-app.component.ts
@@ -1,90 +1,121 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription, interval } from 'rxjs';
+
 @Component({
   selector: 'app-quiz-app',
   templateUrl: './quiz-app.component.html',
   styleUrls: []
 })
-export class QuizAppComponent implements OnInit {
+export class QuizAppComponent implements OnInit, OnDestroy {
 
   showWarning: boolean = false;
-
   isQuizStarted: boolean = false;
   isQuizEnded: boolean = false;
-  questionsList: any[]= [];
+  questionsList: any[] = [];
   currentQuestionNo: number = 0;
-
-  remainingTime:number = 10;
-
-  timer = interval(1000);
-  subscription: Subscription [] = [];
+  remainingTime: number = 10;
+  timerSubscription: Subscription | undefined;
   correctAnswerCount: number = 0;
 
   constructor(private http: HttpClient) { }
 
+  ngOnInit(): void {
+    this.loadQuestions();
+  }
 
+  ngOnDestroy(): void {
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
 
-   ngOnInit(): void { 
-     this.loadQuestions();
-   }
-   loadQuestions() {
-     this.http.get("assets/question.json").subscribe((res:any)=>{
-       debugger;
-       this.questionsList = res;
-     })
-   }
-   nextQuestion() {
-     if(this.currentQuestionNo < this.questionsList.length-1) {
-       this.currentQuestionNo ++;
-     } else {
-       this.subscription.forEach(element => {
-         element.unsubscribe();
-       });
-     } 
-   }
-   finish() {
-     this.isQuizEnded = true;
-     this.isQuizStarted = false;  
-   }
+  loadQuestions() {
+    this.http.get("assets/question.json").subscribe((res: any) => {
+      this.questionsList = res;
+    });
+  }
 
-   start() {
-     this.showWarning = false;
-     this.isQuizEnded = false;
-     this.isQuizStarted = false;  
-   }
+  start() {
+    this.resetQuiz();
+  }
 
-   showWarningPopup() {
-     this.showWarning = true;
-   }
+  resetQuiz() {
+    this.showWarning = false;
+    this.isQuizEnded = false;
+    this.isQuizStarted = false;
+    this.currentQuestionNo = 0;
+    this.correctAnswerCount = 0;
+    this.remainingTime = 10;
 
-   selectOption(option: any) {
-     if(option.isCorrect) {
-       this.correctAnswerCount ++;
-     }
-     option.isSelected = true;
-   }
-   isOptionSelected(options: any) {
-     const selectionCount = options.filter((m:any)=>m.isSelected == true).length;
-     if(selectionCount == 0) {
-       return false;
-     } else {
-       return true;
-     }
-   }
-   startQuiz() {
-     this.showWarning = false;
-     this.isQuizStarted = true;  
-    this.subscription.push(this.timer.subscribe(res=> {
-       console.log(res);
-       if(this.remainingTime != 0) {
-         this.remainingTime --;
-       } 
-       if(this.remainingTime == 0) {
-         this.nextQuestion();
-         this.remainingTime = 10;
-       } 
-     })
-    )
-   }
+    // Reset selected options in each question
+    this.questionsList.forEach((question) => {
+      question.options.forEach((option) => {
+        option.isSelected = false;
+      });
+    });
+  }
+
+  startQuiz() {
+    this.showWarning = false;
+    this.isQuizStarted = true;
+
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+
+    this.timerSubscription = interval(1000).subscribe(() => {
+      if (this.remainingTime > 0) {
+        this.remainingTime--;
+      } else {
+        this.nextQuestion();
+      }
+    });
+  }
+
+  nextQuestion() {
+    if (this.currentQuestionNo < this.questionsList.length - 1) {
+      this.currentQuestionNo++;
+      this.remainingTime = 10; // Restart time for the next question
+    } else {
+      this.finish();
+    }
+  }
+
+  finish() {
+    this.isQuizEnded = true;
+    this.isQuizStarted = false;
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+  quitGame() {
+    this.showWarning = false;
+    this.isQuizStarted = false;
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+  exitGame() {
+    this.resetQuiz();
+    console.log('Quiz reset.');
+  }
+
+  selectOption(option: any) {
+    if (option.isCorrect) {
+      this.correctAnswerCount++;
+    }
+    option.isSelected = true;
+  }
+
+  isOptionSelected(options: any) {
+    const selectionCount = options.filter((option: any) => option.isSelected).length;
+    return selectionCount > 0;
+  }
+
+  showWarningPopup() {
+    this.showWarning = true;
+  }
 }
