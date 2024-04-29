bc46487b7bd0c77354528595dd6c0250e7033ca7
bayelaye313 <bayelaye0510@gmail.com>
2024-04-29 12:26:38 +0000

    v beta

--- my-first-project/src/app/app.module.ts
+++ my-first-project/src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 import { FormsModule } from '@angular/forms';
+import { QuizAppComponent } from './quiz-app/quiz-app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    QuizAppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
